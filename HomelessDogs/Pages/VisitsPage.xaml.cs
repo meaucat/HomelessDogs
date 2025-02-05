72ebc82df99c6e08ec44760522b326a568907225
202109 <202109@WINMAC15>
2025-02-05 14:50:23 +0300

    Добавьте файлы проекта.

---
+++ HomelessDogs/Pages/VisitsPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HomelessDogs.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для VisitsPage.xaml
+    /// </summary>
+    public partial class VisitsPage : Page
+    {
+        public VisitsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EditInformationBTN_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new EditVisitPage());
+        }
+
+        private void BackBTN_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new VisitsPage());
+        }
+    }
+}
